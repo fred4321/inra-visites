8309a3874a9b9a1c22488005b5e4021afac2c431
sbrulin <sbrulin@novelios.com>
2018-11-19 10:25:09 +0100

    Mantis #0000903: Filtre "site"

--- grails-app/controllers/BSController.groovy
+++ grails-app/controllers/BSController.groovy
@@ -78,6 +78,13 @@ class BSController {
                                 ilike('matricule', "${filter.matricule}%")
                             }
                         }
+                        if(filter.site){
+                            agent{
+                                unite{
+                                    ilike('site', "${filter.site}%")
+                                }
+                            }
+                        }
                     }
                     switch (sortIndex){
                         case "unite":
