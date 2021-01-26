5294dd008dc691a0f45edef2f940bb81bf1e17e4
Frédéric Fanien <ffanien@novelios.com>
2021-01-26 21:31:28 +0100

    no message

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,12 +5,12 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
-grails.server.port.http = 8081
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.fork = [
     // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
     //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
     // configure settings for the test-app JVM, uses the daemon by default
     test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
     // configure settings for the run-app JVM
@@ -34,31 +34,45 @@ grails.project.dependency.resolution = {
 
     repositories {
         inherits true // Whether to inherit repository definitions from plugins
+
         grailsPlugins()
         grailsHome()
         mavenLocal()
         grailsCentral()
         mavenCentral()
+        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
     }
 
     dependencies {
-        test "org.grails:grails-datastore-test-support:1.0-grails-2.4"
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
+        // runtime 'mysql:mysql-connector-java:5.1.29'
+        // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
+        test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
         runtime "org.springframework:spring-test:3.1.0.RELEASE"
     }
 
     plugins {
+        // plugins for the build system only
+        build ":tomcat:7.0.55"
+
+        // plugins for the compile step
+        compile ":scaffolding:2.1.2"
+        compile ':cache:1.1.8'
+        compile ":asset-pipeline:1.9.9"
+
+        // plugins needed at runtime but not for compilation
+        runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
+        runtime ":database-migration:1.4.0"
+        runtime ":jquery:1.11.1"
+
         compile ":rendering:1.0.0"
         compile ":mandrill:0.5"
         compile ":export:1.5"
         compile ":mail:1.0.7"
-        // plugins for the build system only
-        build ":tomcat:8.0.14.1"
-        // plugins for the compile step
-        compile ":scaffolding:2.1.2"
-        compile ":cache:1.1.8"
-        compile ":jasypt-encryption:1.3.1"
-        // plugins needed at runtime but not for compilation
-        runtime ":hibernate4:4.3.5.5"
-        runtime ":database-migration:1.4.0"
+        //compile ':jasypt-encryption:1.3.1'
     }
 }
+grails.plugin.location.'jasypt-encryption' = "plugins/jasypt-encryption-1.3.1"
