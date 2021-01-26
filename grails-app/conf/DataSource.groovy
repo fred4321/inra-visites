16668cec14614762123642c5aa918f5ff1f4acf2
Frédéric Fanien <ffanien@novelios.com>
2021-01-26 22:03:43 +0100

    no message

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -29,13 +29,32 @@ environments {
     }
     test { // toulouse
         dataSource {
-            dbCreate = "update" // one of 'create', 'create-drop','update'
-            url = "jdbc:mysql://127.0.0.1/inra?autoReconnect=true"
-			
+            dbCreate = "update"
+            url = "jdbc:mysql://localhost/inra?autoReconnect=true"
             pooled=true
             driverClassName="com.mysql.jdbc.Driver"
-            username="root"
-            password="crer313225"
+            username="inra"
+            password="inra313225"
+            properties {
+                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+                jmxEnabled = true
+                initialSize = 5
+                maxActive = 50
+                minIdle = 5
+                maxIdle = 25
+                maxWait = 10000
+                maxAge = 10 * 60000
+                timeBetweenEvictionRunsMillis = 5000
+                minEvictableIdleTimeMillis = 60000
+                validationQuery = "SELECT 1"
+                validationQueryTimeout = 3
+                validationInterval = 15000
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = false
+                jdbcInterceptors = "ConnectionState"
+                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+            }
         }
     }
     production { // rennes
